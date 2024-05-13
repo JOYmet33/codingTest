fe0190e77b86d5409ffbf86462283174543bb435
joymet33 <joymet33@gmail.com>
2024-05-14 00:33:42 +0900

    work: 2024. 5. 14.

--- 20240515.js
+++ 20240515.js
@@ -1,1 +1,23 @@
 // 수박수박수박수박수박수?
+
+//1차 풀이
+function solution(n) {
+  var answer = "";
+  for (let i = 0; i < n; i++) {
+    if (i % 2 !== 0) {
+      answer = answer + "박";
+    } else {
+      answer = answer + "수";
+    }
+  }
+  return answer;
+}
+
+// 2차 풀이 - 삼항 연산자 활용
+function solution(n) {
+  var answer = "";
+  for (let i = 0; i < n; i++) {
+    i % 2 !== 0 ? (answer += "박") : (answer += "수");
+  }
+  return answer;
+}
--- 20240517.js
+++ 20240517.js
@@ -1,1 +1,14 @@
 //약수의 개수와 덧셈
+function solution(left, right) {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    let arr = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        arr += 1;
+      }
+    }
+    arr % 2 === 0 ? (answer += i) : (answer -= i);
+  }
+  return answer;
+}
